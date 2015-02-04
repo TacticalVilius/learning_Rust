298fb18a9bc7bbfcd20c02d713c22560be0060cf
Vilius Kazakauskas <v.kazakauskas@sportradar.com>
2015-02-04 15:26:39 +0100

    Player input and validation

--- mastermind/src/main.rs
+++ mastermind/src/main.rs
@@ -1,25 +1,49 @@
 use std::rand;
+use std::old_io;
+
+enum PlayerInput {
+	ValidInput(String),
+	ErrorReason(String)
+}
 
 fn main() {
     println!("Welcome to MASTERMIND");
 	println!("Available colours: G B Y R");
 	println!("");
 	
-	let mut answer = [""; 4];
-	for i in 0u32..4 {
-		answer[i] = "a";
+	let mut answer: Vec<&str> = Vec::new();
+	for i in 0..4 {
+		answer.push(get_random_colour_letter());
 	}
 	
-	print!("Answer is ");
+	print!("Answer is: ");
 	for element in answer.iter() {
 		print!("{}", element);
 	}
-	println!("\n");
+	println!("");
 	
 	let mut counter: i32 = 1;
 	
 	loop {
-		print!("{}", counter);
+		print!("{}...\t\t", counter);
+		
+		let input = old_io::stdin()
+					.read_line()
+					.ok()
+					.expect("Failed to read player input");
+		
+		let guess = match validate_player_input(input.trim()) {
+			PlayerInput::ErrorReason(error_string) => {
+				println!("{}", error_string);
+				continue;
+			},
+			PlayerInput::ValidInput(guess) => {
+				guess
+			}
+		};
+		
+		
+		
 		counter = counter + 1;
 		if counter > 10 {return;}
 	};
@@ -39,5 +63,25 @@ fn unsigned_int_to_colour_letter(i: u32) -> &'static str {
 			println!("ERROR: Cannot convert unsigned integer {} to a colour", i);
 			""
 		}
+	}
+}
+
+fn validate_player_input(input: &str) -> PlayerInput {
+	if input.len() != 4 { PlayerInput::ErrorReason("Length of guess must be 4".to_string()) }
+	else {
+		for l in input.graphemes(true) {
+			if valid_colour_letter(l) == false { return PlayerInput::ErrorReason(format!("Invalid colour letter {}", l)); }
+		}
+		PlayerInput::ValidInput(input.to_string())
+	}
+}
+
+fn valid_colour_letter(letter: &str) -> bool {
+	match letter {
+		"G" => true,
+		"B" => true,
+		"Y" => true,
+		"R" => true,
+		_ => false
 	}
 }
